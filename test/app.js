897db5cde8e3b07d49c1a77270d696bf8abf80c9
obermiller.k <obermiller.k@husky.neu.edu>
2017-05-11 16:04:42 -0400

    Fixed MLab Setup

--- test/app.js
+++ test/app.js
@@ -4,14 +4,11 @@ module.exports = function(app)
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
-    var connectionString = '@ds137271.mlab.com:37271/heroku_k795p235';
+    var connectionString = 'mongodb://127.0.0.1:27017/test';
 
     if(process.env.MLAB_USERNAME) {
         connectionString = process.env.MLAB_USERNAME + ":" +
-            process.env.MLAB_PASSWORD + "@" +
-            process.env.MLAB_HOST + ':' +
-            process.env.MLAB_PORT + '/' +
-            process.env.MLAB_APP_NAME;
+            process.env.MLAB_PASSWORD + "@ds137271.mlab.com:37271/heroku_k795p235";
     }
 
     var mongoose = require("mongoose");
