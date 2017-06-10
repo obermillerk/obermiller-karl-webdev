2511295c5c592c37d767ae1a756f29368a81ef8c
obermiller.k <obermiller.k@husky.neu.edu>
2017-06-10 01:42:30 -0400

    Fixed mLab

--- test/app.js
+++ test/app.js
@@ -9,7 +9,7 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
+    connectionString += '@ds137271.mlab.com:37271/heroku_k795p235'; // user yours
 }
 
 var mongoose = require("mongoose");
